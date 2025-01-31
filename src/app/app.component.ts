3468e7fe50d550e8d171a714fb25581b354f326e
Gabriel Detofol <detofol29@gmail.com>
2025-01-31 00:11:45 -0300

    Adicionando home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from './componentes/menu/menu.component';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from "./componentes/home/home.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MenuComponent, CommonModule],
+  imports: [RouterOutlet, MenuComponent, CommonModule, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
